1dbe1cdcc217b346d1c209ede05e40d1f2c0c45c
gerald <oforige@gmail.com>
2022-12-16 08:11:10 +0000

    restructured the login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Register from "./components/register";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/register" exact component={Register} />
-          {/*<Route path="/customers />*/}
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+
         </Routes>
       </Router>
   );
---
+++ client/src/components/auth/login.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+
+export default function Login() {
+    return (
+        <div>
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center align-items-center">
+                    <Col md={8} lg={6} xs={12}>
+                        <Card className="px-4">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 className="fw-bold mb-2 text-center text-uppercase ">
+                                        Logo
+                                    </h2>
+                                    <div className="mb-3">
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="email">
+                                                <Form.Label className="text-center">Email</Form.Label>
+                                                <Form.Control type="text" placeholder="Enter Name" />
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3" controlId="password">
+                                                <Form.Label className="text-center">Password</Form.Label>
+                                                <Form.Control type="text" placeholder="Enter Name" />
+                                            </Form.Group>
+
+
+                                            <div className="d-grid">
+                                                <Button variant="primary" type="submit">
+                                                    Login
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                        <div className="mt-3">
+                                            <p className="mb-0  text-center">
+                                                Don't have an account?{' '}
+                                                <a href="client/src/components#" className="text-primary fw-bold">
+                                                    Register
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
